abe73712f9b20a9a6087a5ba5bcf08a184c33cf5
sjb18516558202-coder <sjb18516558202@gmail.com>
2026-08-17 23:08:25 +0800

    上传个人源码、PCB原理图、3D结构模型

--- 2026电赛/M0/1309MSP例程/MSPM0G3507_Motor_SingleSpeed/SeekFree_MSPM0G3507_Opensource_Library/project/code/motor.c
+++
@@ -1,7 +0,0 @@
-#include "motor.h"
-
-/*
- * 当前文件预留给后续扩展使用。
- * 目前电机闭环、速度设置、PID 设置等函数都放在 motorsetspeed.c 中。
- * 具体调用方法请查看：Motor_Call_Guide_CN.md
- */
--- 2026电赛/M0/M0/MSP_Motor_Ctrl/motor_crc.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOTOR_CRC_h
-#define MOTOR_CRC_h
-
-#include "ti_msp_dl_config.h"
-
-unsigned short CRC16(uint8_t *puchMsg, unsigned short usDataLen); //”√”⁄º∆À„ CRC
-
-
-#endif
